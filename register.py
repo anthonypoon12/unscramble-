4fa59a2d2a6fc7c79562730647c6350562270c68
apoon10 <35239699+apoon10@users.noreply.github.com>
2019-05-28 19:22:05 -0400

    Add files via upload

---
+++ register.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+import cgi
+import cgitb
+form=cgi.FieldStorage()
+cgitb.enable()
+wrong=''
+username=form.getvalue('username','')
+password=form.getvalue('password','')
+allusernames=[]
+allpasswords=[]
+f=open('usernames.txt','r')
+allusernames=f.read()
+f.close()
+allusernames=allusernames.split('\n')
+if allusernames[-1]=='':
+    allusernames==allusernames[:-1]
+f=open('passwords.txt','r')
+allpasswords=f.read()
+f.close()
+allpasswords=allpasswords.split('\n')
+if allpasswords[-1]=='':
+    allpasswords==allpasswords[:-1]
+if username=='' or password=='':
+    wrong='Remember to type in a username and password'
+else:
+    if username in allusernames:
+        wrong='USERNAME ALREADY TAKEN'
+if wrong=='':
+    f=open("usernames.txt","a")
+    f.write('\n' + username)
+    f.close()
+    f=open('passwords.txt','a')
+    f.write('\n' + password)
+    f.close()
+    wrong='IT WORKS' + username + password
+print('Content-type: text/html\n')
+print('''
+<html>
+<body>
+%s
+</body>
+</html>
+''' % wrong)
+    
