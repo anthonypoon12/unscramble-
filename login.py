0d82b6b83f95c128d949dc916ae2b2df3aa44068
apoon10 <35239699+apoon10@users.noreply.github.com>
2019-05-28 19:09:23 -0400

    Add files via upload

---
+++ login.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+import cgi
+import cgitb
+form=cgi.FieldStorage()
+cgitb.enable()
+text=''
+wrong=''
+username=form.getvalue('username','')
+password=form.getvalue('password','')
+if username=='' or password=='':
+    wrong='Remember to type in your username and password'
+username=='***'+username+'***'
+password=='***'+username+'***'
+allusernames=''
+allpasswords=''
+f=open('usernames.txt','r')
+allusernames=f.read()
+f.close()
+allusernames=allusernames.split('\n')
+if allusernames[-1]=='':
+    allusernames==allusernames[:-1]
+f=open('passwords.txt','r')
+allpasswords=f.read()
+f.close()
+allpasswords=allpasswords.split('\n')
+if allpasswords[-1]=='':
+    allpasswords==allpasswords[:-1]
+if username in allusernames:
+    if password==allpasswords[allusernames.index(username)]:
+        wrong='IT WORKS'
+    else:
+        wrong='WRONG PASSWORD'
+else:
+    wrong='WRONG USERNAME/PASSWORD'
+print('Content-type: text/html\n')
+print('''
+<html>
+<body>
+%s
+</body>
+</html>
+''' % wrong)
+    
